169450fe8297bc530078c6393e11ae85c9a147c4
erey21 <arinerey@yandex.ru>
2021-11-04 21:51:53 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace homework_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание № 4");
+            Console.WriteLine("Введите сумму вклада: ");
+            double sum = Convert.ToDouble(Console.ReadLine());
+            if (sum < 100)
+            {
+                Console.WriteLine("Сумма вклада с начисленными процентами:");
+                Console.WriteLine(sum += sum * 0.05);
+            }
+            else if (sum < 200)
+            {
+                Console.WriteLine("Сумма вклада с начисленными процентами:");
+                Console.WriteLine(sum += sum * 0.07);
+            }
+            else {
+                Console.WriteLine("Сумма вклада с начисленными процентами:");
+                Console.WriteLine(sum += sum * 0.1);
+            }
+        }
+    }
+}
